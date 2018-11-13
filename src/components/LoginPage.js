f9e618ec58229b0b0d9e4cd9ad8a4098aca56852
william-mcdermott <williammcdermott@Williams-MacBook-Pro.local>
2018-11-13 13:06:16 -0500

    Initial commit

---
+++ src/components/LoginPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { startLogin } from '../actions/auth';
+
+export const LoginPage = ({ startLogin }) => (
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1 className="box-layout__title">Bill's social app</h1>
+      <p>Project for LaunchPartner</p>
+      <button onClick={startLogin} className="button">Login with Google</button>
+    </div>
+  </div>
+);
+
+const mapDispatchToProps = (dispatch) => ({
+  startLogin: () => dispatch(startLogin())
+});
+
+export default connect(undefined, mapDispatchToProps)(LoginPage);
